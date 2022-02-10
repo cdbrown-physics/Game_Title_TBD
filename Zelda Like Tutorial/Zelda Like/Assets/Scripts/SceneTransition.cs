a39eb9b4daa1afaadb4245f5cee1d6ba92fa5148
Ninja-of-Physics <62783943+Ninja-of-Physics@users.noreply.github.com>
2022-02-09 18:29:41 -0700

    Transitions added, done with part 29

--- Zelda Like Tutorial/Zelda Like/Assets/Scripts/SceneTransition.cs
+++ Zelda Like Tutorial/Zelda Like/Assets/Scripts/SceneTransition.cs
@@ -8,12 +8,39 @@ public class SceneTransition : MonoBehaviour
     public string sceneToLoad;
     public Vector2 playerPosition;
     public VectorValue playerStorage;
+    public GameObject fadeInPanel;
+    public GameObject fadeOutPanel;
+    public float fadeWait;
+
+    public void Awake()
+    {
+        if (fadeInPanel != null)
+        {
+            GameObject panel = Instantiate(fadeInPanel, Vector3.zero, Quaternion.identity);
+            Destroy(panel, 1);
+        }
+    }
     public void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Player") && !collision.isTrigger)
         {
             playerStorage.initialValue = playerPosition;
-            SceneManager.LoadScene(sceneToLoad);
+            StartCoroutine(FadeCo());
         }
+    }
+
+    public IEnumerator FadeCo()
+    {
+        if (fadeOutPanel != null)
+        {
+            Instantiate(fadeOutPanel, Vector3.zero, Quaternion.identity);
+        }
+        yield return new WaitForSeconds(fadeWait);
+        AsyncOperation asyncOperation = SceneManager.LoadSceneAsync(sceneToLoad);
+        while(!asyncOperation.isDone)
+        {
+            yield return null;
+        }
+        
     }
 }
